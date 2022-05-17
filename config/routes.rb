749948f4e55708d943ba4d312ea11f60424a7fec
naoyuki <hoshizorarequest@gmail.com>
2022-05-18 01:05:56 +0900

    check_sorcery

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root 'posts#index'
+
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+
+  get 'login' => 'user_sessions#new'
+  post 'login' => "user_sessions#create"
+  delete 'logout' => 'user_sessions#destroy'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_16_062704) do
+ActiveRecord::Schema.define(version: 2022_05_17_131640) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
